33ff13eb37612da90e7fb23b7e523a4331b5a95f
Esianyo <esianicd@gmail.com>
2023-05-09 22:08:31 -0700

    correction to 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for com in range(10):
     for second in range(com + 1, 10):
-        print("{:02d}, ".format(com * 10 + second), end=" ")
+        print("{:d}{:d}".format(com, second), end="")
+        if com == 8 and second == 9:
+            print()
+        else:
+            print(", ", end="")
