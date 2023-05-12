c73185862abfbfb0cceee470bfdadbcec4ddda5b
Esianyo <esianicd@gmail.com>
2023-05-11 18:06:11 -0700

    making a script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+args = sys.argv[1:]
+num_args = len(args)
+
+if num_args == 0:
+    print("0 arguments")
+    print(".")
+else:
+    print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
+    for num, argz in enumerate(args):
+        print("{}: {}".format(num + 1, argz))
