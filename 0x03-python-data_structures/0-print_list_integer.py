5c9244839257412ce9ee79b31a0c316f7b9d076f
Esianyo <esianicd@gmail.com>
2023-05-15 17:20:04 -0700

    printing integers using str.format()

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,5 +6,4 @@ print_list_integer - prints all integers of a list
 
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if isinstance(i, int):
-            print("{}\n".format(i), end="")
+        print("{}\n".format(i), end="")
