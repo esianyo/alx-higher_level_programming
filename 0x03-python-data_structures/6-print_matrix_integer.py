2c415c8b68441dbc7fa49098c2d851fae3785cec
Esianyo <esianicd@gmail.com>
2023-05-16 22:15:31 -0700

    python data structures: tuple and list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for m in matrix:
         for i in m:
-            print(i, end=" ")
+            print("{:3d}".format(i), end="")
         print()
