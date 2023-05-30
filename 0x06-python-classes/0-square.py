ce3a1ae4ab9e6546dff2c9008edd9d8fafe425e2
Esianyo Dzisenu <esianicd@gmail.com>
2023-05-30 09:31:03 -0700

    An empty python class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
-"""This is a python class named Square"""
+"""
+This class defines a square.
+
+Attributes:
+  side (int): The side length of the square.
+
+Methods:
+  get_area(): Returns the area of the square.
+  get_perimeter(): Returns the perimeter of the square.
+"""
 
 class Square:
-    pass
+  """
+  This class defines a square.
+
+  Args:
+    side (int): The side length of the square.
+  """
+  pass
