32af06fb376dedc1114b9698d31fcf6b01983bd5
Esianyo Dzisenu <esianicd@gmail.com>
2023-06-05 16:26:23 -0700

    tasks 0 and 1 - rectangle classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Defines an empty rectangle class"""
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+class Rectangle:
+    """Defines the rectangle"""
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("heght must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.width = width
+        self.height = height
