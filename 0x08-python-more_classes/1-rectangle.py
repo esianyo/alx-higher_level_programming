62b3bac09ffd5666173de58536fce5f2927ab7b3
Esianyo Dzisenu <esianicd@gmail.com>
2023-06-05 18:45:05 -0700

    corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,70 +1,21 @@
+add standard python documentation
+
 #!/usr/bin/python3
-
-
+"""
+Rectangle class
+Esianyo Dzisenu
+"""
 class Rectangle:
-    """
-    Rectangle class
-    """
-
+    """Defines the rectangle"""
     def __init__(self, width=0, height=0):
-        """
-        Initialising
-
-        Args:
-           width (int, optional): the width of the rectangle
-           height (int, optional): the height of the rectangle
-        """
-        self.__width = width
-        self.__height = height
-
-    @property
-    def width(self):
-        """
-        Width getter
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        """
-        Width setter
-
-        Args:
-           width (int, optional): the width of the rectangle
-
-        Raises:
-            TypeError: if width is not an integer
-            ValueError: if width is not >= 0
-        """
-
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
-
-    @property
-    def height(self):
-        """
-        Height getter
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        """
-        Height setter
-
-        Args:
-           height (int, optional): the height of the rectangle
-
-        Raises:
-            TypeError: if height is not an integer
-            ValueError: if height is not >= 0
-        """
-
         if not isinstance(height, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("heght must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+
+        self.height = height
+        self. Width = width
