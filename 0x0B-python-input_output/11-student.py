b0f0804e464ed9d71784bb60956e3bda3699ddf0
Esianyo Dzisenu <esianicd@gmail.com>
2023-06-13 22:22:52 -0700

    up to task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,39 +3,33 @@
 
 
 class Student:
+    """Represents a student  """
+
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a Student object."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+    
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
+        """Returns a dictionary representation of the Student object."""
 
-        Args:
-            attrs (list): List of attribute.
+        if attrs is None:
+            attrs = []
 
-        Returns:
-            dict: Dictionary representation of the Student instance.
-        """
-        if attrs is None or not isinstance(attrs, list):
-            return self.__dict__
+            json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
 
-        return {
-                attr: getattr(self, attr)
-                for attr in attrs
-                if hasattr(self, attr)
-                }
+        return json_dict
 
     def reload_from_json(self, json):
-        """
-        Replaces all attributes of the Student instance.
+        """Replaces all attributes of the Student instance."""
 
-        Args:
-            json (dict): Dictionary containing attribute names and values.
-
-        Returns:
-            None
-        """
-        for key, value in json.items():
-            setattr(self, key, value)
+        for attr, value in json.items():
+            if hasattr(self, attr):
+                setattr(self, attr, value)
