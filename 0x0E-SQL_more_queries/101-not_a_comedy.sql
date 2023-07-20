bc37d8c58d925280be87ef943e38c68d8a803334
esianyo <esianicd@gmail.com>
2023-07-20 14:45:01 +0000

    updating

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,7 @@
 --  lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name IS NULL OR tv_genres.name != 'Comedy'
+LEFT JOIN shows_genres ON tv_shows.id = shows_genres.show_id
+LEFT JOIN genres ON shows_genres.genre_id = genres.id
+WHERE genres.name != 'Comedy' OR genres.name IS NULL
 ORDER BY tv_shows.title ASC;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,6 @@
+SELECT tv_genres.name, SUM(rating) AS rating_sum
+FROM tv_genres
+LEFT JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
