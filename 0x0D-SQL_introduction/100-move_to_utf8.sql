126ffec0df290c956e8f4842c3f417f8b620a995
esianyo <esianicd@gmail.com>
2023-10-18 01:07:44 +0000

    updated utf

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,5 @@ ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the field to UTF8MB4
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
