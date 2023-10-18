8261b86114068123d46dd0f8665001d6759aa16c
esianyo <esianicd@gmail.com>
2023-10-18 01:58:45 +0000

    task 17 updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,5 @@
 -- Convert the database to UTF8MB4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8MB4
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert the field to UTF8MB4
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
