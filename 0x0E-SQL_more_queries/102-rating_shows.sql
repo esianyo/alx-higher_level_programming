58c0aac14b25814d9a246903655cff43b5618c1d
esianyo <esianicd@gmail.com>
2023-10-19 01:39:05 +0000

    task 19 updated

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
---  lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) as rating_sum
+-- List all shows by their rating
+SELECT CONCAT(tv_shows.title, ' - ', SUM(tv_show_ratings.rating)) AS show_rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY SUM(tv_show_ratings.rating) DESC;
