3d54172468f4b8b4802f8fd4d0ec63b66707a4f6
esianyo <esianicd@gmail.com>
2023-11-15 20:32:45 +0000

    my weirdest commit message...lol

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-let a = process.argv[2];
-let b = process.argv[3];
-
-a = parseInt(a);
-b = parseInt(b);
-
 function add (a, b) {
-  return a + b;
+  console.log(parseInt(a) + parseInt(b));
 }
-
-if (isNaN(a) || isNaN(b)) {
-  console.log('Both arguments must be integers');
-} else {
-  console.log(add(a, b));
-}
+add(process.argv[2], process.argv[3]);
