dabb267ac1ad9dafe1ceccf4a7db72f6392a89f8
esianyo <esianicd@gmail.com>
2023-11-15 19:17:56 +0000

    debugged

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 let sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; len = sentence.length; i < len, i++){
+len = sentence.length;
+let i = 0;
+for (; i < len; i++) {
     console.log(sentence[i]);
 }
