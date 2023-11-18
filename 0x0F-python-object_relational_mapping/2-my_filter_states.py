dd24dc9e561e33f7dd8bb16b7439d90be1b326b6
esianyo <esianicd@gmail.com>
2023-11-18 20:34:28 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script to list in a filtered manner"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    un = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    hN = 'localhost'
+    
+    db = MySQLdb.cursor.execute("SELECT * FROM states WHERE\
+                 name LIKE BINARY '{:s}' ORDER BY id ASC".format(argv[4]))
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` WHERE \
+                   name LIKE BINARY 'N%' ORDER BY states.id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close
+    db.close
