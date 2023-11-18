a20da743c440706b8ac181895adbc027cf7b537f
esianyo <esianicd@gmail.com>
2023-11-18 20:00:17 +0000

    filtering table

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script to list all states from a database"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    un = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    hN = 'localhost'
+
+    db = MySQLdb.connect(host=hN, port=3306, user=un, password=pwd, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT FROM `states` WHERE \ NAME LIKE BINARY 'N%' ORDER BY states.id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close
+    db.close
