8db9f7aff001fad827d78f8274751134dba17ebc
esianyo <esianicd@gmail.com>
2023-11-18 21:15:19 +0000

    Edudzi, keep laughing at me wai

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""scripts that lists all states"""
+"""a script to list in a filtered manner"""
 
-from sys import argv
+
 import sys
+from sys import argv
 import MySQLdb
 
+
 if __name__ == "__main__":
-
-    u_name = sys.argv[1]
-    p = sys.argv[2]
+    un = sys.argv[1]
+    pwd = sys.argv[2]
     db_name = sys.argv[3]
-    h = 'localhost'
+    hN = 'localhost'
 
-    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    db = MySQLdb.connect(host=hN, port=3306, user=un, password=pwd, db=db_name)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE \
-                   name LIKE BINARY '{:s}' ORDER BY id ASC".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE\
+                   name LIKE BINARY '{:s}'\
+                   ORDER BY id ASC".format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
