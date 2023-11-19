062c2236a8ea7017d35f81c57891966ae0525bd0
esianyo <esianicd@gmail.com>
2023-11-19 21:47:09 +0000

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Check the number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./list_states_containing_a.py <mysql_username>\
+              <mysql_password> <database_name>")
+        exit(1)
+
+    # Connect to the MySQL database
+    engine = create_engine(f"mysql://{sys.argv[1]}:\
+                           {sys.argv[2]}@localhost:3306/{sys.argv[3]}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database for states containing the letter "a"
+    states_containing_a = (
+        session.query(State)
+        .filter(State.name.contains("a")
+                )
+        .order_by(State.id)
+    )
+
+    # Print the results
+    for state in states_containing_a:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
