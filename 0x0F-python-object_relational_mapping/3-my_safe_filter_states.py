4a31a285cc46c24c66eaf1e3fd623fcb3901b7c3
esianyo <esianicd@gmail.com>
2023-11-19 20:04:43 +0000

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+    cursor = db.cursor()
+
+    # Use MySQLdb's parameterized query to avoid MySQL injections
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+    
+    # Display the results
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
