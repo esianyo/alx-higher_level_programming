64908c166ffac8c3eacc89cec97d2b09b7bf30fa
esianyo <esianicd@gmail.com>
2023-11-19 20:43:10 +0000

    debugged

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
+"""Selects all cities by state"""
+
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-def get_states(username, password, db_name):
-    '''
-        lists all cities from the database.
-    '''
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         db=db_name,
-                         port=3306)
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database,
+        host="localhost",
+        port=3306,
+    )
 
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name\
+
+    cursor.execute("SELECT * FROM cities")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        city_id = row[0]
+        city_name = row[1]
+        state_id = row[2]
+
+        cursor.execute("SELECT cities.id, cities.name, states.name\
                     FROM `cities`\
                     JOIN `states` ON state_id=states.id\
                     ORDER BY cities.id")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+        state_name = cursor.fetchone()[0]
+
+        print(f"City: {city_name}, State: {state_name}")
+
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    credentials = sys.argv
-    username = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    get_states(username, passwd, db_name)
