d63b416dd6e92dcdbb053f11638204ab923a44f2
esianyo <esianicd@gmail.com>
2023-12-03 21:44:32 +0000

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers"""
+    if not list_of_integers:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    if length == 2:
+        return max(list_of_integers)
+
+    mid = length // 2
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    if list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    return list_of_integers[mid]
