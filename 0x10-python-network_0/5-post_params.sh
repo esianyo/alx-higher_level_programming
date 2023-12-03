346148b57b712aebc13ce60e381e39ea80796ff8
esianyo <esianicd@gmail.com>
2023-12-03 21:28:52 +0000

    curl POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL headers
-curl -s -X "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# cURL POST
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
