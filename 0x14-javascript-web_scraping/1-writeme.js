c787562eccdbf04439a8366d92f7287e748cac01
esianyo <esianicd@gmail.com>
2024-01-03 18:28:11 +0000

    这是 task 0

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const path = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(path, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`String written to file ${path}`);
+  }
+});
