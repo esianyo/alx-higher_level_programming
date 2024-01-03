00d5e543bb0c1627298fecd66c37978c6067c955
esianyo <esianicd@gmail.com>
2024-01-03 18:12:52 +0000

    这是 task 0

--- 0-readme.js
+++ 0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const path = process.argv[2];
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(path, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
