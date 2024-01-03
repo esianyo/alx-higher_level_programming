fd2448362d7b650ca487f89f637dd0bac7d11ed5
esianyo <esianicd@gmail.com>
2024-01-03 18:20:09 +0000

    这是 task 0

--- 0-readme.js
+++ 0-readme.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
+const path = process.argv[2];
 
-if (!filePath) {
-  console.log('Usage: node script_name.js file_path');
-  process.exit(1);
-}
-
-try {
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-  process.stdout.write(fileContent + '\n');
-} catch (error) {
-  console.error(`An error occurred while reading the file: ${error}`);
-}
+fs.readFile(path, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
