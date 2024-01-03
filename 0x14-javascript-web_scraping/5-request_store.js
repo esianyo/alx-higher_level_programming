d4e1284a4b654651be2312a0120b9b285d583af8
esianyo <esianicd@gmail.com>
2024-01-03 19:17:05 +0000

    这是 task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFileSync(filePath, body, 'utf-8');
+  }
+});
