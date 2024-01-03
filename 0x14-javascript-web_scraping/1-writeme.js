25d8cba7ea7374ee276b7d4d87a1f8ed85a8d8cf
esianyo <esianicd@gmail.com>
2024-01-03 18:48:37 +0000

    这是 task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = process.argv[2];
-const content = process.argv[3];
 
-fs.writeFile(path, content, 'utf-8', (err) => {
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(err);
   }
 });
