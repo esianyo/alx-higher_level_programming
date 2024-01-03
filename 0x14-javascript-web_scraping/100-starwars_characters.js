4be0bc7e14f5740ec9af1d09a5c4b03daaf20135
esianyo <esianicd@gmail.com>
2024-01-03 19:22:14 +0000

    这是 advance task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(charError);
+        } else {
+          const character = JSON.parse(charBody);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
